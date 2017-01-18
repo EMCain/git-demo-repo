37d89b4f1464de7667f85be4949b2b5f921f37d6
Emily C <emilymargaretcain@gmail.com>
2017-01-17 17:00:12 -0800

    Implemented Hello World and FizzBuzz.

--- hello.py
+++ hello.py
@@ -1,5 +1,13 @@
 # a very sophisticated algorithm to print 'hello world'
 
-
 print("~*~*~*~ ohai world !! ^_^  ~*~*~*~")
 
+# bonus fizzbuzz
+
+for i in range(25):
+    st = "  "
+    if i % 3 == 0:
+        st += "fizz"
+    if i % 5 == 0:
+        st += "buzz"
+    print(str(i) + st)
